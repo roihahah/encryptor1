50279e6eab89672b15bdeb554eb1a56674e84227
Roy Hadad <roihahah73@gmail.com>
2026-03-15 14:11:30 +0200

    Add documentation for the crypto service class (CryptoService)

--- src/main/java/service/CryptoService.java
+++ src/main/java/service/CryptoService.java
@@ -5,15 +5,28 @@ import java.util.Random;
 public class CryptoService {
     public int key;
 
+
+    /**
+     *creating a random integer from 1-10
+     * @return an integer
+     */
     public int createKey(){
+        //initializing a java Random object
         Random rand = new Random();
         return rand.nextInt(10) + 1;
     }
+
+    /**
+     * given byte array (data) and an integer (key) then encrypting the data by adding the key to each byte
+     * @param data the data to encrypt
+     * @param key the key for the encryption
+     * @return a byte array of the encrypted data
+     */
     public byte[] encrypt(byte[] data , int key) {
-
-
+        //initializing an empty byte array for the result of the encryption
         byte[] result = new byte[data.length];
 
+        //adding for each byte the key and putting it in the appropriate element of the result array
         for (int i = 0; i < data.length; i++) {
             result[i] = (byte) (data[i] + key);
         }
@@ -21,9 +34,18 @@ public class CryptoService {
         return result;
     }
 
+    /**
+     * given byte array (data) and an integer (key) and then decrypting the data by subtracting the key to each byte
+     *
+     * @param data the data to decrypt
+     * @param key the key for the decryption
+     * @return a byte array of the decrypted data
+     */
     public byte[] decrypt(byte[] data, int key) {
+        //initializing an empty byte array for the result of the decryption
         byte[] result = new byte[data.length];
 
+        //subtracting for each byte the key and putting it in the appropriate element of the result array
         for (int i = 0; i < data.length; i++) {
             result[i] = (byte) (data[i] - key);
         }
