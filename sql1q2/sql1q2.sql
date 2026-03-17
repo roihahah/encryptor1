d261bc82f4e1ba0bd4f802a60af1655c14e57681
Roy Hadad <roihahah73@gmail.com>
2026-03-17 16:35:07 +0200

    Fix indentation

--- sql1q2/sql1q2.sql
+++ sql1q2/sql1q2.sql
@@ -1,10 +1,10 @@
 --2
 --a
-  SELECT students.first_name , students.last_name
-    FROM RoyHadad_Students students
-    JOIN RoyHadad_Schools schools
-      ON schools.id = students.school_id
-ORDER BY schools.name , students.first_name , students.last_name;
+SELECT students.first_name , students.last_name
+  FROM RoyHadad_Students students
+  JOIN RoyHadad_Schools schools
+    ON schools.id = students.school_id
+ ORDER BY schools.name , students.first_name , students.last_name;
 
 --b
 SELECT s.score , e.exam_date
@@ -36,9 +36,9 @@ SELECT settlements.area, settlements.name , schools.name
     ON settlements.id = schools.settlement_id;
 
 --f
-  SELECT students.id , students.first_name , students.last_name , AVG(scores.score) AS average_score
-    FROM RoyHadad_Students students
-    JOIN RoyHadad_Scores scores
-      ON scores.student_id = students.id
-   WHERE students.first_name = 'דפנה'
-GROUP BY students.id , students.first_name , students.last_name;
+SELECT students.id , students.first_name , students.last_name , AVG(scores.score) AS average_score
+  FROM RoyHadad_Students students
+  JOIN RoyHadad_Scores scores
+    ON scores.student_id = students.id
+ WHERE students.first_name = 'דפנה'
+ GROUP BY students.id , students.first_name , students.last_name;
