07c93f5d97400f0bab625d44953acef07c182a40
Roy Hadad <roihahah73@gmail.com>
2026-03-17 16:12:06 +0200

    Fix indention and table insertion

--- sql1q3/inserts.sql
+++ sql1q3/inserts.sql
@@ -1,7 +1,7 @@
-INSERT INTO RoyHadad_Settlements (settlement, area) VALUES ('תל אביב', 'מרכז');
-INSERT INTO RoyHadad_Settlements (settlement, area) VALUES ('חיפה', 'צפון');
-INSERT INTO RoyHadad_Settlements (settlement, area) VALUES ('ירושלים', 'מרכז');
-INSERT INTO RoyHadad_Settlements (settlement, area) VALUES ('באר שבע', 'דרום');
+INSERT INTO RoyHadad_Settlements (name, area) VALUES ('תל אביב', 'מרכז');
+INSERT INTO RoyHadad_Settlements (name, area) VALUES ('חיפה', 'צפון');
+INSERT INTO RoyHadad_Settlements (name, area) VALUES ('ירושלים', 'מרכז');
+INSERT INTO RoyHadad_Settlements (name, area) VALUES ('באר שבע', 'דרום');
 
 INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('בית הספר לאומניות', 1);
 INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('בית הספר העירוני א', 2);
