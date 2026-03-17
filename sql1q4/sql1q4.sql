8da02468783be414c4b4e728ead98543fceb9bee
Roy Hadad <roihahah73@gmail.com>
2026-03-17 16:31:48 +0200

    Fix indentation

--- sql1q4/sql1q4.sql
+++ sql1q4/sql1q4.sql
@@ -3,35 +3,35 @@
 
 --b
 DELETE FROM RoyHadad_Scores
-WHERE exam_id IN (
+ WHERE exam_id IN (
     SELECT id
-    FROM RoyHadad_Exams
-    WHERE field = 'אנגלית'
-)
-AND score < 80;
+      FROM RoyHadad_Exams
+     WHERE field = 'אנגלית'
+  )
+   AND score < 80;
 
 --c
 UPDATE RoyHadad_Scores
-SET score = GREATEST(score -3 ,0)
-WHERE exam_id IN (
+   SET score = GREATEST(score -3 ,0)
+ WHERE exam_id IN (
     SELECT id
-    FROM RoyHadad_Exams
-    WHERE field = 'אנגלית'
+      FROM RoyHadad_Exams
+     WHERE field = 'אנגלית'
 );
 
 --d
 UPDATE RoyHadad_Scores
-SET score = LEAST(score +3 , 100)
-WHERE exam_id IN (
+   SET score = LEAST(score +3 , 100)
+ WHERE exam_id IN (
     SELECT id
-    FROM RoyHadad_Exams
-    WHERE field = 'מתמטיקה'
+      FROM RoyHadad_Exams
+     WHERE field = 'מתמטיקה'
 );
 
 --end
 CREATE OR REPLACE VIEW RoyHadad_north_schools AS
 SELECT school.id , school.name
-FROM RoyHadad_Schools school
-JOIN RoyHadad_Settlements settlements
+  FROM RoyHadad_Schools school
+  JOIN RoyHadad_Settlements settlements
     ON school.settlement_id = settlements.id
-WHERE settlements.area = 'צפון';
+ WHERE settlements.area = 'צפון';
