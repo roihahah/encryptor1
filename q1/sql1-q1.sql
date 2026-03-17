766e43e6f86b427282e94d2837c8dd58b62433a9
Roy Hadad <roihahah73@gmail.com>
2026-03-17 15:44:11 +0200

    Fix comments

--- q1/sql1-q1.sql
+++ q1/sql1-q1.sql
@@ -1,26 +1,26 @@
 
 --1
 --a
-SELECT first_name , last_name 
-FROM RoyHadad_Students;
+SELECT  first_name, last_name 
+  FROM  RoyHadad_Students;
 
 --b
-SELECT name 
-FROM RoyHadad_Schools;
+SELECT  name 
+FROM    RoyHadad_Schools;
 
 --c
-SELECT s.score , e.field 
-FROM RoyHadad_Exams e
-JOIN RoyHadad_Scores s
+SELECT s.score, e.field 
+  FROM RoyHadad_Exams e
+  JOIN RoyHadad_Scores s
     ON e.id = s.exam_id
-WHERE s.score > 55;
+ WHERE s.score > 55;
 
 --d
 SELECT first_name || ' ' || last_name AS full_name
-FROM RoyHadad_Students
-WHERE last_name LIKE 'ב%';
+  FROM RoyHadad_Students
+ WHERE last_name LIKE 'ב%';
 
 --e
-SELECT first_name , last_name 
-FROM RoyHadad_Students
-ORDER BY first_name , last_name ASC;
+  SELECT first_name, last_name 
+    FROM RoyHadad_Students
+ORDER BY first_name, last_name ASC;
