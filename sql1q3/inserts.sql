cbf439ec02d18025e2cc160c3329af41e4bf60e3
Roy Hadad <roihahah73@gmail.com>
2026-03-18 09:41:40 +0200

    Fix comments based on prior feedback

--- sql1q3/inserts.sql
+++ sql1q3/inserts.sql
@@ -1,111 +1,120 @@
-INSERT INTO RoyHadad_Settlements (name, area) VALUES ('תל אביב', 'מרכז');
-INSERT INTO RoyHadad_Settlements (name, area) VALUES ('חיפה', 'צפון');
-INSERT INTO RoyHadad_Settlements (name, area) VALUES ('ירושלים', 'מרכז');
-INSERT INTO RoyHadad_Settlements (name, area) VALUES ('באר שבע', 'דרום');
+-- Areas
+INSERT INTO Roy_Hadad_Areas (id, name) VALUES (areas_seq.NEXTVAL, 'מרכז');
+INSERT INTO Roy_Hadad_Areas (id, name) VALUES (areas_seq.NEXTVAL, 'צפון');
+INSERT INTO Roy_Hadad_Areas (id, name) VALUES (areas_seq.NEXTVAL, 'דרום');
 
-INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('בית הספר לאומניות', 1);
-INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('בית הספר העירוני א', 2);
-INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('בית הספר העירוני ב', 3);
-INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('תיכון הדרום', 4);
-INSERT INTO RoyHadad_Schools (name, settlement_id) VALUES ('בית ספר ללא כימיה', 1);
+-- Settlements
+INSERT INTO Roy_Hadad_Settlements (id, name, area_id) VALUES (settlements_seq.NEXTVAL, 'תל אביב', 1);
+INSERT INTO Roy_Hadad_Settlements (id, name, area_id) VALUES (settlements_seq.NEXTVAL, 'חיפה', 2);
+INSERT INTO Roy_Hadad_Settlements (id, name, area_id) VALUES (settlements_seq.NEXTVAL, 'ירושלים', 1);
+INSERT INTO Roy_Hadad_Settlements (id, name, area_id) VALUES (settlements_seq.NEXTVAL, 'באר שבע', 3);
 
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (1, 'דפנה', 'כהן');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (1, 'יואב', 'לוי');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (1, 'מאיה', 'ברק');
+-- Schools
+INSERT INTO Roy_Hadad_Schools (id, name, settlement_id) VALUES (schools_seq.NEXTVAL, 'בית הספר לאומניות', 1);
+INSERT INTO Roy_Hadad_Schools (id, name, settlement_id) VALUES (schools_seq.NEXTVAL, 'בית הספר העירוני א', 2);
+INSERT INTO Roy_Hadad_Schools (id, name, settlement_id) VALUES (schools_seq.NEXTVAL, 'בית הספר העירוני ב', 3);
+INSERT INTO Roy_Hadad_Schools (id, name, settlement_id) VALUES (schools_seq.NEXTVAL, 'תיכון הדרום', 4);
+INSERT INTO Roy_Hadad_Schools (id, name, settlement_id) VALUES (schools_seq.NEXTVAL, 'בית ספר ללא כימיה', 1);
 
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (2, 'נועה', 'ישראלי');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (2, 'איתי', 'מלכה');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (2, 'שירה', 'ביטון');
+-- Students
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 1, 'דפנה', 'כהן');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 1, 'יואב', 'לוי');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 1, 'מאיה', 'ברק');
 
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (3, 'רן', 'אברהם');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (3, 'תמר', 'דוד');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (3, 'אופק', 'שלום');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 2, 'נועה', 'ישראלי');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 2, 'איתי', 'מלכה');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 2, 'שירה', 'ביטון');
 
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (4, 'גל', 'פרץ');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (4, 'ליאור', 'בן דוד');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 3, 'רן', 'אברהם');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 3, 'תמר', 'דוד');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 3, 'אופק', 'שלום');
 
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (5, 'עדי', 'חכם');
-INSERT INTO RoyHadad_Students (school_id, first_name, last_name) VALUES (5, 'נועם', 'מור');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 4, 'גל', 'פרץ');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 4, 'ליאור', 'בן דוד');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (1, 'כימיה', 'מבחן כימיה א', DATE '2024-01-10');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 5, 'עדי', 'חכם');
+INSERT INTO Roy_Hadad_Students (id, school_id, first_name, last_name) VALUES (students_seq.NEXTVAL, 5, 'נועם', 'מור');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (1, 'מתמטיקה', 'מבחן מתמטיקה א', DATE '2024-02-10');
+-- Exams
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 1, 'כימיה', 'מבחן כימיה א', DATE '2024-01-10');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (1, 'ביולוגיה', 'מבחן ביולוגיה א', DATE '2024-03-10');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 1, 'מתמטיקה', 'מבחן מתמטיקה א', DATE '2024-02-10');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (2, 'ביולוגיה', 'מבחן ביולוגיה ב', DATE '2024-01-12');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 1, 'ביולוגיה', 'מבחן ביולוגיה א', DATE '2024-03-10');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (2, 'כימיה', 'מבחן כימיה ב', DATE '2024-02-14');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 2, 'ביולוגיה', 'מבחן ביולוגיה ב', DATE '2024-01-12');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (2, 'מתמטיקה', 'מבחן מתמטיקה ב', DATE '2024-03-15');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 2, 'כימיה', 'מבחן כימיה ב', DATE '2024-02-14');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (3, 'מתמטיקה', 'מבחן מתמטיקה ג', DATE '2024-01-18');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 2, 'מתמטיקה', 'מבחן מתמטיקה ב', DATE '2024-03-15');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (3, 'כימיה', 'מבחן כימיה ג', DATE '2024-02-20');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 3, 'מתמטיקה', 'מבחן מתמטיקה ג', DATE '2024-01-18');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (4, 'כימיה', 'מבחן כימיה ד', DATE '2024-01-25');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 3, 'כימיה', 'מבחן כימיה ג', DATE '2024-02-20');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (4, 'היסטוריה', 'מבחן היסטוריה ד', DATE '2024-03-01');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 4, 'כימיה', 'מבחן כימיה ד', DATE '2024-01-25');
 
-INSERT INTO RoyHadad_Exams (school_id, field, name, exam_date)
-VALUES (5, 'ספרות', 'מבחן ספרות ה', DATE '2024-02-01');
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 4, 'היסטוריה', 'מבחן היסטוריה ד', DATE '2024-03-01');
 
+INSERT INTO Roy_Hadad_Exams (id, school_id, field, name, exam_date)
+VALUES (exams_seq.NEXTVAL, 5, 'ספרות', 'מבחן ספרות ה', DATE '2024-02-01');
+
+-- Scores
 -- בית הספר לאומניות
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (1, 1, 95);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (2, 1, 88);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (3, 1, 91);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (1, 1, 95);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (2, 1, 88);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (3, 1, 91);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (1, 2, 76);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (2, 2, 82);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (3, 2, 79);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (1, 2, 76);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (2, 2, 82);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (3, 2, 79);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (1, 3, 89);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (2, 3, 93);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (3, 3, 90);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (1, 3, 89);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (2, 3, 93);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (3, 3, 90);
 
 -- בית הספר העירוני א
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (4, 4, 84);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (5, 4, 87);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (6, 4, 90);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (4, 4, 84);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (5, 4, 87);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (6, 4, 90);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (4, 5, 72);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (5, 5, 69);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (6, 5, 75);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (4, 5, 72);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (5, 5, 69);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (6, 5, 75);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (4, 6, 93);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (5, 6, 96);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (6, 6, 94);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (4, 6, 93);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (5, 6, 96);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (6, 6, 94);
 
 -- בית הספר העירוני ב
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (7, 7, 91);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (8, 7, 85);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (7, 7, 91);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (8, 7, 85);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (7, 8, 77);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (8, 8, 81);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (7, 8, 77);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (8, 8, 81);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (7, 9, 95);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (8, 9, 92);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (7, 9, 95);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (8, 9, 92);
 
 -- תיכון הדרום
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (9, 10, 68);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (10, 10, 74);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (9, 10, 68);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (10, 10, 74);
 
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (9, 11, 83);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (10, 11, 79);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (9, 11, 83);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (10, 11, 79);
 
 -- בית ספר ללא כימיה
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (11, 12, 88);
-INSERT INTO RoyHadad_Scores (exam_id, student_id, score) VALUES (11, 13, 73);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (11, 12, 88);
+INSERT INTO Roy_Hadad_Scores (exam_id, student_id, score) VALUES (11, 13, 73);
 
 COMMIT;
-
--- sql1q3/sql1q3.sql
+++ sql1q3/sql1q3.sql
@@ -1,10 +1,10 @@
 
 --a
 SELECT schools.id, students.id, students.first_name, students.last_name, ROUND(AVG(scores.score),2) AS avg_score
-  FROM RoyHadad_Students students
-  JOIN RoyHadad_Schools schools
+  FROM Roy_Hadad_Students students
+  JOIN Roy_Hadad_Schools schools
     ON students.school_id = schools.id
-  JOIN RoyHadad_Scores scores
+  JOIN Roy_Hadad_Scores scores
     ON students.id = scores.student_id
  WHERE schools.name = 'בית הספר לאומניות'
  GROUP BY 
@@ -16,10 +16,10 @@ SELECT schools.id, students.id, students.first_name, students.last_name, ROUND(A
 
 --b
 SELECT schools.id, schools.name, ROUND(AVG(scores.score), 2) AS avg_score
-  FROM RoyHadad_Schools schools
-  JOIN RoyHadad_Exams exams
+  FROM Roy_Hadad_Schools schools
+  JOIN Roy_Hadad_Exams exams
     ON schools.id = exams.school_id
-  JOIN RoyHadad_Scores scores
+  JOIN Roy_Hadad_Scores scores
     ON scores.exam_id = exams.id
  GROUP BY 
     schools.id,
@@ -27,20 +27,20 @@ SELECT schools.id, schools.name, ROUND(AVG(scores.score), 2) AS avg_score
 
 --c
 SELECT schools.id, ROUND(AVG(scores.score), 2) AS avg_score
-  FROM RoyHadad_Schools schools
-  JOIN RoyHadad_Exams exams
+  FROM Roy_Hadad_Schools schools
+  JOIN Roy_Hadad_Exams exams
     ON schools.id = exams.school_id
-  JOIN RoyHadad_Scores scores
+  JOIN Roy_Hadad_Scores scores
     ON scores.exam_id = exams.id
  WHERE schools.name = 'בית הספר העירוני א' AND exams.field = 'ביולוגיה'
  GROUP BY schools.id;
 
 --d
 SELECT schools.id, schools.name, ROUND(AVG(scores.score), 2) AS avg_score
-  FROM RoyHadad_Schools schools
-  JOIN RoyHadad_Exams exams
+  FROM Roy_Hadad_Schools schools
+  JOIN Roy_Hadad_Exams exams
     ON schools.id = exams.school_id
-  JOIN RoyHadad_Scores scores
+  JOIN Roy_Hadad_Scores scores
     ON scores.exam_id = exams.id 
  WHERE exams.field = 'כימיה'
  GROUP BY schools.id, schools.name
@@ -48,11 +48,11 @@ SELECT schools.id, schools.name, ROUND(AVG(scores.score), 2) AS avg_score
 
 --e
 SELECT schools.id, schools.name, ROUND(AVG(scores.score), 2) AS avg_score
-  FROM RoyHadad_Schools schools
-  LEFT JOIN RoyHadad_Exams exams
+  FROM Roy_Hadad_Schools schools
+  LEFT JOIN Roy_Hadad_Exams exams
     ON exams.school_id = schools.id
    AND exams.field = 'כימיה'
-  LEFT JOIN RoyHadad_Scores scores
+  LEFT JOIN Roy_Hadad_Scores scores
     ON scores.exam_id = exams.id
  GROUP BY schools.id, schools.name
  ORDER BY avg_score DESC;
@@ -60,8 +60,8 @@ SELECT schools.id, schools.name, ROUND(AVG(scores.score), 2) AS avg_score
 --f
 
 SELECT students.id, students.first_name, students.last_name, ROUND(AVG(scores.score) , 2) AS avg_score
-  FROM RoyHadad_Students students
-  JOIN RoyHadad_Scores scores
+  FROM Roy_Hadad_Students students
+  JOIN Roy_Hadad_Scores scores
     ON scores.student_id = students.id
  GROUP BY students.id, students.first_name, students.last_name
  ORDER BY avg_score DESC
@@ -70,12 +70,12 @@ SELECT students.id, students.first_name, students.last_name, ROUND(AVG(scores.sc
 
 --g
 SELECT students.id, students.first_name, students.last_name, ROUND(AVG(scores.score), 2) AS avg_score
-  FROM RoyHadad_Students students
-  JOIN RoyHadad_Schools schools
+  FROM Roy_Hadad_Students students
+  JOIN Roy_Hadad_Schools schools
     ON schools.id = students.school_id
-  JOIN RoyHadad_Scores scores
+  JOIN Roy_Hadad_Scores scores
     ON students.id = scores.student_id
-  JOIN RoyHadad_Exams exams
+  JOIN Roy_Hadad_Exams exams
     ON exams.id = scores.exam_id
  WHERE exams.field = 'מתמטיקה' AND schools.name = 'בית הספר העירוני ב'
  GROUP BY students.id, students.first_name, students.last_name
