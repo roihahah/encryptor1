84e26946870aac00067a9a0765faf337c3e6dada
Roy Hadad <roihahah73@gmail.com>
2026-03-19 15:36:32 +0200

    Fix comments and naming

--- sql2q2/sql2q2.sql
+++ sql2q2/sql2q2.sql
@@ -1,34 +1,34 @@
 --a + b
-ALTER TABLE Roy_Hadad_Settlements
-ADD status NUMBER DEFAULT 1;
+ALTER TABLE roy_hadad_settlements
+  ADD status NUMBER DEFAULT 1;
 
 --c
-CREATE OR REPLACE PACKAGE Roy_Hadad_city_updater AS
-    PROCEDURE Roy_Hadad_update_city;
+CREATE OR REPLACE PACKAGE roy_hadad_city_updater AS
+    PROCEDURE roy_hadad_update_city;
 END Roy_Hadad_city_updater; 
   
-CREATE OR REPLACE PACKAGE BODY Roy_Hadad_city_updater AS 
-    PROCEDURE Roy_Hadad_update_city AS
+CREATE OR REPLACE PACKAGE BODY roy_hadad_city_updater AS 
+    PROCEDURE roy_hadad_update_city AS
     BEGIN
-        UPDATE Roy_Hadad_Settlements
+        UPDATE roy_hadad_settlements
            SET status = 2
          WHERE status = 1;
          
          COMMIT;
-    END Roy_Hadad_update_city;
-END Roy_Hadad_city_updater;
+    END roy_hadad_update_city;
+END roy_hadad_city_updater;
 
 
 --d
 BEGIN
-    DBMS_SCHEDULER.drop_job('ROYHADAD_JOB');
+    DBMS_SCHEDULER.drop_job('UPDATE_SETTLEMENT_STATUS_JOB');
 END;
 
 BEGIN
     DBMS_SCHEDULER.create_job(
         job_name => 'UPDATE_SETTLEMENT_STATUS_JOB',
         job_type => 'PLSQL_BLOCK',
-        job_action => 'BEGIN Roy_Hadad_city_updater.Roy_Hadad_update_city; END;',
+        job_action => 'BEGIN roy_hadad_city_updater.roy_hadad_update_city; END;',
         start_date => SYSTIMESTAMP,
         repeat_interval => 'FREQ=SECONDLY; INTERVAL=30',
         enabled => TRUE
