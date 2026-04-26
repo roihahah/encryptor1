cbfd37c78c889dcce22f9d772c97ee787ad1adf3
Roy Hadad <roihahah73@gmail.com>
2026-04-26 12:55:13 +0300

    Encryptor 1 Switch To Switch And Separate Encryption And Decryption Logic Into Different Functions

--- src/main/java/Application/EncryptionApplication.java
+++ src/main/java/Application/EncryptionApplication.java
@@ -6,6 +6,7 @@ import service.CryptoService;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class EncryptionApplication {
@@ -17,36 +18,47 @@ public class EncryptionApplication {
         System.out.println("-- Welcome to the most program of programs --");
         System.out.println("Enter " + ENCRYPT_OPTION + " for Encryption of a file or " + DECRYPT_OPTION + " for Decryption and press enter to confirm: ");
         String userChoice =  (scanner.nextLine()).toUpperCase();
-
-        if (userChoice.equals(ENCRYPT_OPTION))
+        switch (userChoice)
         {
-            System.out.println("Welcome to the encryption");
-
-            CryptoService cryptoService = new CryptoService();
-            FileHandlingEncryptor fileHandler = new FileHandlingEncryptor();
-
-            Path path = fileHandler.getSourceFilePath(scanner);
-            byte[] data = fileHandler.getFileData(path);
-
-            int key = cryptoService.createKey();
-            byte[] encryptedData = cryptoService.encrypt(data , key);
-
-            fileHandler.saveEncryptedData(encryptedData , path , key);
+            case "E":
+                runEncryptionProgram(scanner);
+                break;
+            case "D":
+                runDecryptionProgram(scanner);
+                break;
+            default:
+                System.out.println("Unrecognizable Option");
         }
-        else if (userChoice.equals(DECRYPT_OPTION))
-        {
-            System.out.println("Welcome to the decryption");
 
-            CryptoService cryptoService = new CryptoService();
-            FileHandlingDecryptor fileHandler = new FileHandlingDecryptor();
+    }
+    public static void runEncryptionProgram(Scanner scanner) throws IOException
+    {
+        System.out.println("Welcome to the encryption");
 
-            Path path = fileHandler.getSourceFilePath(scanner);
-            byte[] data = fileHandler.getFileData(path);
+        CryptoService cryptoService = new CryptoService();
+        FileHandlingEncryptor fileHandler = new FileHandlingEncryptor();
 
-            int key = fileHandler.getKey(path);
-            byte[] decryptedData = cryptoService.decrypt(data , key);
+        Path path = fileHandler.getSourceFilePath(scanner);
+        byte[] data = fileHandler.getFileData(path);
 
-            fileHandler.saveDecryptedData(decryptedData , path);
-        }
+        int key = cryptoService.createKey();
+        byte[] encryptedData = cryptoService.encrypt(data , key);
+
+        fileHandler.saveEncryptedData(encryptedData , path , key);
+    }
+    public static void runDecryptionProgram(Scanner scanner) throws IOException
+    {
+        System.out.println("Welcome to the decryption");
+
+        CryptoService cryptoService = new CryptoService();
+        FileHandlingDecryptor fileHandler = new FileHandlingDecryptor();
+
+        Path path = fileHandler.getSourceFilePath(scanner);
+        byte[] data = fileHandler.getFileData(path);
+
+        int key = fileHandler.getKey(path);
+        byte[] decryptedData = cryptoService.decrypt(data , key);
+
+        fileHandler.saveDecryptedData(decryptedData , path);
     }
 }
--- src/main/java/service/CryptoService.java
+++ src/main/java/service/CryptoService.java
@@ -5,21 +5,12 @@ import java.util.Random;
 public class CryptoService
 {
     public int key;
-    /**
-     *creating a random integer from 1-10
-     * @return an integer
-     */
+
     public int createKey()
     {
         Random rand = new Random();
         return rand.nextInt(10) + 1;
     }
-    /**
-     * given byte array (data) and an integer (key) then encrypting the data by adding the key to each byte
-     * @param data the data to encrypt
-     * @param key the key for the encryption
-     * @return a byte array of the encrypted data
-     */
     public byte[] encrypt(byte[] data , int key)
     {
         byte[] result = new byte[data.length];
@@ -29,13 +20,6 @@ public class CryptoService
         }
         return result;
     }
-    /**
-     * given byte array (data) and an integer (key) and then decrypting the data by subtracting the key to each byte
-     *
-     * @param data the data to decrypt
-     * @param key the key for the decryption
-     * @return a byte array of the decrypted data
-     */
     public byte[] decrypt(byte[] data, int key)
     {
         byte[] result = new byte[data.length];
