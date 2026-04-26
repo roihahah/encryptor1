1fd2a0eba85faef3054c5ab101efa6c2c2d1834e
Roy Hadad <roihahah73@gmail.com>
2026-04-26 11:46:46 +0300

    Encryptor 1 Create EncryptionApplication For Running All The Application Stuff

---
+++ src/main/java/Application/EncryptionApplication.java
@@ -0,0 +1,52 @@
+package Application;
+
+import file.FileHandlingDecryptor;
+import file.FileHandlingEncryptor;
+import service.CryptoService;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class EncryptionApplication {
+    private static final String ENCRYPT_OPTION = "E";
+    private static final String DECRYPT_OPTION = "D";
+
+    public static void run() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("-- Welcome to the most program of programs --");
+        System.out.println("Enter " + ENCRYPT_OPTION + " for Encryption of a file or " + DECRYPT_OPTION + " for Decryption and press enter to confirm: ");
+        String userChoice =  (scanner.nextLine()).toUpperCase();
+
+        if (userChoice.equals(ENCRYPT_OPTION))
+        {
+            System.out.println("Welcome to the encryption");
+
+            CryptoService cryptoService = new CryptoService();
+            FileHandlingEncryptor fileHandler = new FileHandlingEncryptor();
+
+            Path path = fileHandler.getSourceFilePath(scanner);
+            byte[] data = fileHandler.getFileData(path);
+
+            int key = cryptoService.createKey();
+            byte[] encryptedData = cryptoService.encrypt(data , key);
+
+            fileHandler.saveEncryptedData(encryptedData , path , key);
+        }
+        else if (userChoice.equals(DECRYPT_OPTION))
+        {
+            System.out.println("Welcome to the decryption");
+
+            CryptoService cryptoService = new CryptoService();
+            FileHandlingDecryptor fileHandler = new FileHandlingDecryptor();
+
+            Path path = fileHandler.getSourceFilePath(scanner);
+            byte[] data = fileHandler.getFileData(path);
+
+            int key = fileHandler.getKey(path);
+            byte[] decryptedData = cryptoService.decrypt(data , key);
+
+            fileHandler.saveDecryptedData(decryptedData , path);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,52 +1,10 @@
-import file.FileHandlingDecryptor;
-import file.FileHandlingEncryptor;
-import service.CryptoService;
+import Application.EncryptionApplication;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Scanner;
 
 public class Main
 {
-    private static final String ENCRYPT_OPTION = "E";
-    private static final String DECRYPT_OPTION = "D";
-
-    public static void main(String[] args) throws IOException
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("-- Welcome to the most program of programs --");
-        System.out.println("Enter " + ENCRYPT_OPTION + " for Encryption of a file or " + DECRYPT_OPTION + " for Decryption and press enter to confirm: ");
-        String userChoice =  (scanner.nextLine()).toUpperCase();
-
-        if (userChoice.equals(ENCRYPT_OPTION))
-        {
-            System.out.println("Welcome to the encryption");
-
-            CryptoService cryptoService = new CryptoService();
-            FileHandlingEncryptor  fileHandler = new FileHandlingEncryptor();
-
-            Path path = fileHandler.getSourceFilePath(scanner);
-            byte[] data = fileHandler.getFileData(path);
-
-            int key = cryptoService.createKey();
-            byte[] encryptedData = cryptoService.encrypt(data , key);
-
-            fileHandler.saveEncryptedData(encryptedData , path , key);
-        }
-        else if (userChoice.equals(DECRYPT_OPTION))
-        {
-            System.out.println("Welcome to the decryption");
-
-            CryptoService cryptoService = new CryptoService();
-            FileHandlingDecryptor fileHandler = new FileHandlingDecryptor();
-
-            Path path = fileHandler.getSourceFilePath(scanner);
-            byte[] data = fileHandler.getFileData(path);
-
-            int key = fileHandler.getKey(path);
-            byte[] decryptedData = cryptoService.decrypt(data , key);
-
-            fileHandler.saveDecryptedData(decryptedData , path);
-        }
+    public static void main(String[] args) throws IOException {
+        EncryptionApplication.run();
     }
 }
