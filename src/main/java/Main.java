a78b491a3db39817fef1a83a2df605ea2bbab1a5
Roy Hadad <roihahah73@gmail.com>
2026-04-27 09:58:26 +0300

    Encryptor 1 Reorganize Stuff 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
 import Application.EncryptionApplication;
 
-import java.io.IOException;
-
 public class Main
 {
     public static void main(String[] args)
--- src/main/java/file/FileHandling.java
+++ src/main/java/file/FileHandling.java
@@ -5,7 +5,6 @@ import service.UserInputService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Scanner;
 
 public class FileHandling
 {
