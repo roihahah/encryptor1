72284ddb0d66ab2388a097566316feebc2fab18e
Roy Hadad <roihahah73@gmail.com>
2026-05-25 09:22:30 +0300

    Encryptor 2 Change Reverse To Use Existing Function

--- src/main/java/encryption/EncryptionKey.java
+++ src/main/java/encryption/EncryptionKey.java
@@ -1,23 +1,13 @@
 package encryption;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 public record EncryptionKey(int[] keys)
 {
     public EncryptionKey reverse()
     {
         int[] reversedKeys = keys.clone();
-
-        int left = 0;
-        int right = reversedKeys.length - 1;
-
-        while (left < right)
-        {
-            int temp = reversedKeys[left];
-            reversedKeys[left] = reversedKeys[right];
-            reversedKeys[right] = temp;
-
-            left++;
-            right--;
-        }
+        ArrayUtils.reverse(reversedKeys);
 
         return new EncryptionKey(reversedKeys);
     }
